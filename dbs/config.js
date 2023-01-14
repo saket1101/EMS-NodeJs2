b4206981577343fc6f9ec854c92568ba2d1f9ace
Saket Kumar <saketjha00@gmail.com>
2023-01-14 12:16:37 +0530

    config

--- dbs/config.js
+++ dbs/config.js
@@ -1,9 +1,1 @@
-require('dotenv').config()
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zpvgcjo.mongodb.net/?retryWrites=true&w=majority`;
-mongoose
-  .connect(url)
-  .then(() => console.log("Db connected"))
-  .catch((err) => console.log("error", err.message));
--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 //database connection
-const dbConnect = require("./dbs/config")
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zpvgcjo.mongodb.net/?retryWrites=true&w=majority`;
+mongoose
+  .connect(url)
+  .then(() => console.log("Db connected"))
+  .catch((err) => console.log("error", err.message));
+
 
 // required routers
 const adminRouter = require("./routers/adminRoutes");
